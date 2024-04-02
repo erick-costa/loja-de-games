8d9c38627da2ea293405542b0cb54d7933e92fac
erick-costa <erickcosta2509@gmail.com>
2024-04-02 16:04:48 -0300

    CRUD produto

---
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,61 @@
+package com.generation.lojadegames.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 2, max = 100, message = "O atributo título deve conter no mínimo 02 e no máximo 100 caracteres!")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo descricao é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo descricao deve conter no mínimo 10 e no máximo 1000 caracteres!")
+	private String descricao;
+	
+	@NotNull(message = "O atributo valor é obrigatório!")
+	private float valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+}
